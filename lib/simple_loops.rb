d52129e9e075836008b5fc311c5dbdf615218cf2
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-29 05:14:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(message = "Hello world")
     counter += 1 
   end
 end
+
+def loop_message_n_times([message = "hello world", 10])
+  counter = 0 
+  while counter <= [1] do
+    puts message
+    counter += 1
+  end
+end
