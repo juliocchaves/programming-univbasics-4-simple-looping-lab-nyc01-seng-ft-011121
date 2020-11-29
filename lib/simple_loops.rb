940c4faa904b9576d3f456218192de33a399292f
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-29 05:37:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,10 +27,12 @@ end
 quote = ["5", "4", "3", "2", "1"]
 def return_string_array(quote)
   count = 0 
+  new_array = []
   while count < quote.length do
-    quote[count].to_s
+    new_array.push(quote[count].to_s)
     count +=1 
   end
+  new_array
 end
 
 
